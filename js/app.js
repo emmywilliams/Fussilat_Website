7085b88d7a191447e58ca1b1fd3eeb583c079300
Emmy Williams <emmy.kayanja@students.mak.ac.ug>
2023-08-14 09:39:10 +0300

    emmy williams

--- js/app.js
+++ js/app.js
@@ -59,10 +59,51 @@ $(document).ready(function () {
 
 function closeNavbar() {
   if (window.innerWidth <= 992) {
-    // Adjust this breakpoint as needed
     let navbar = document.querySelector(".navbar-collapse");
     if (navbar.classList.contains("show")) {
       navbar.classList.remove("show");
     }
   }
 }
+
+// Function to start the animation when the section is in the viewport
+function startMilestoneAnimation() {
+  const milestoneSection = document.getElementById("milestone");
+  const valueDisplays = milestoneSection.querySelectorAll(".display-4");
+  const interval = 4000;
+
+  valueDisplays.forEach((valueDisplay) => {
+    const endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    const duration = interval / endValue;
+    let startValue = 0;
+
+    const counter = setInterval(() => {
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+      if (startValue === endValue) {
+        clearInterval(counter);
+      }
+    }, duration);
+  });
+
+  // Remove scroll event listener once animation is triggered
+  window.removeEventListener("scroll", startMilestoneAnimation);
+}
+
+// Add a scroll event listener to start the animation when the section is in view
+window.addEventListener("scroll", () => {
+  const milestoneSection = document.getElementById("milestone");
+  const sectionRect = milestoneSection.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  if (sectionRect.top <= windowHeight * 0.75 && sectionRect.bottom >= 0) {
+    startMilestoneAnimation();
+  }
+});
+
+// initialize library
+var input = document.querySelector("#phone");
+window.intlTelInput(input, {
+  preferredCountries: ["ug"],
+  separateDialCode: true,
+});
